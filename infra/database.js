5cc41690252709236c2383bf1a7db2eb542621a1
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2025-07-10 17:35:17 +0000

    #6 deixa o `database.js` mais robusto ao lidar com erros

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,17 @@ async function query(queryObj) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
+
   await client.connect();
-  const result = await client.query(queryObj);
-  client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObj);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
