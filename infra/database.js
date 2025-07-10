0e391bade297aeec6acfa0d375835db1c2cfafa9
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2025-07-10 23:23:22 +0000

    adiciona `console.log()` das informações  do banco de dados

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(queryObj) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObj);
