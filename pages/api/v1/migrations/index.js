649e52e174bcd3683fefee2357f628eb5ee7f81e
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2025-07-17 21:07:39 +0000

    conserta o bug de conexão do endpoint `api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,7 +6,7 @@ export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
-      error: `Method "${request.method}" not allowed."`,
+      error: `Method "${request.method}" not allowed.`,
     });
   }
 
