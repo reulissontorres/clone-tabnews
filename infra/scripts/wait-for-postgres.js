9949f0a6c8d819d34d51cff163d981e6932df129
Reulisson Torres <115098329+reulissontorres@users.noreply.github.com>
2025-07-27 01:13:05 +0000

    #9 fix `eslint` linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n-> PG está pronto e aceitando conexões\/");
+    console.log("\n-> PG está pronto e aceitando conexões\n");
   }
 }
 
